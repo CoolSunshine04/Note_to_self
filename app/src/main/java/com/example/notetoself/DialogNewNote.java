dc4eec416bc9369321cdd77afcb3d30f455fc9e9
CoolSunshine04 <ivan.ponosov.2002@mail.ru>
2020-02-27 16:12:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/notetoself/DialogNewNote.java
@@ -0,0 +1,17 @@
+package com.example.notetoself;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.fragment.app.DialogFragment;
+
+public class DialogNewNote extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        View dialogView = inflater.inflate(R.layout.dialog_new_note,null);
